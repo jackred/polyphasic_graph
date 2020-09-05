07fc21e47c1282eff117bc6acddd4e9ca0895a99
Jackred <jackred@tuta.io>
2020-09-05 23:40:20 +0100

    ADD request data to url
    url is in config.json file

---
+++ src/request_data.py
@@ -0,0 +1,11 @@
+import requests
+from utility import read_json, write_json
+
+def get_json():
+    config = read_json("../config.json")        
+    res = requests.get(config['url']).json()
+    return res
+
+if __name__ == '__main__':
+    data = get_json()
+    write_json(data, '../data/data.json')
